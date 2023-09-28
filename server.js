ac0f48f1b0d098af1f2660874bfc081ca1a06fb3
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-09-28 11:48:45 -0500

    agregue router

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', (req, res) => {
-  res.send('Hola');
+app.use(router);
+
+router.get('/message', (req, res) => {
+  res.send('Lista de mensajes');
 });
+
+router.post('/message', (req, res) => {
+  res.send('Mensaje añadido');
+});
+
+// app.use('/', (req, res) => {
+//   res.send('Hola');
+// });
 
 app.listen(3000);
 console.log('La aplicación está escuchando en http://localhost:3000');
