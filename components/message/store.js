99c2e29f8ac127eba4e3532110ad7750d359fd41
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-09-30 11:09:23 -0500

    agregar esquema de datos para guardar y consultar mensajes

--- components/message/store.js
+++ components/message/store.js
@@ -1,14 +1,25 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+
+db.Promise = global.Promise;
+db.connect(`mongodb+srv://wilmaracaicedos:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.lipiocn.mongodb.net/?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+});
+console.log('[db] Conectada con éxito');
 
 function addMessage(message) {
-  list.push(message);
+  // list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessage() {
-  return list;
+async function getMessages() {
+  // return list;
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
   add: addMessage,
-  list: getMessage,
+  list: getMessages,
 }
