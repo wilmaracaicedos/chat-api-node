1f7022ec52b6980c8772bbaccbe0c3b28bb498aa
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-10-02 17:03:20 -0500

    guardar usuario

---
+++ components/user/network.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const response = require('../../network/response');
+const controller = require('./controller');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  controller.addUser(req.body.name)
+    .then((data) => {
+      response.success(req, res, data, 201);
+    })
+    .catch((e) => {
+      response.error(req, res, 'Internal error', 500, e);
+    });
+});
+
+module.exports = router;
