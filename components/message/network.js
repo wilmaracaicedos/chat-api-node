72c4cde45d231dc0f3311aac9c185206399a25f8
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-10-02 19:59:49 -0500

    recibir fichero desde un mensaje

--- components/message/network.js
+++ components/message/network.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const multer = require('multer');
 const response = require('../../network/response');
 const controller = require('./controller');
 const router = express.Router();
+
+const upload = multer({
+  dest: 'uploads/'
+});
 
 router.get('/', (req, res) => {
   const filterMessages = req.query.user || null;
@@ -14,7 +19,7 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
+router.post('/', upload.single('file'), (req, res) => {
   controller.addMessage(req.body.chat, req.body.user, req.body.message)
     .then((fullMessage) => {
       response.success(req, res, fullMessage, 201);
