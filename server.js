08bb8c7f57a8297ab026a939e040320b4a916f91
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-10-03 17:20:43 -0500

    conectar la api al servidor de webSockets

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
 const bodyParser = require('body-parser');
+const socket = require('./socket');
 require('dotenv').config();
-
 const db = require('./db');
-
 const router = require('./network/routes');
 
 db(`mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.lipiocn.mongodb.net/?retryWrites=true&w=majority`);
 
-var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+socket.connect(server);
 
 router(app);
 
 app.use('/app', express.static('public'));
 
-app.listen(3000);
-console.log('La aplicación está escuchando en http://localhost:3000');
+server.listen(3000, () => {
+  console.log(`La aplicación está escuchando en http://localhost:3000`);
+});
