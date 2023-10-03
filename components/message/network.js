215c453dbd3c7cdd22377dc8615928770dd3e489
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-10-03 11:05:30 -0500

    guardar el fichero en el servidor

--- components/message/network.js
+++ components/message/network.js
@@ -5,7 +5,7 @@ const controller = require('./controller');
 const router = express.Router();
 
 const upload = multer({
-  dest: 'uploads/'
+  dest: 'public/files/'
 });
 
 router.get('/', (req, res) => {
@@ -20,7 +20,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', upload.single('file'), (req, res) => {
-  controller.addMessage(req.body.chat, req.body.user, req.body.message)
+  controller.addMessage(req.body.chat, req.body.user, req.body.message, req.file)
     .then((fullMessage) => {
       response.success(req, res, fullMessage, 201);
     })
